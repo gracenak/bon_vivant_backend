ba53f20b4e08b3bf7a2e7f6044cb2b6f4183b4e6
gracenak <gracenak@gmail.com>
2021-04-21 15:35:53 -0700

    create routes and update controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :reviews
-  resources :recipes
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :users
+      resources :reviews
+      resources :recipes
+    end
+  end
 end
+

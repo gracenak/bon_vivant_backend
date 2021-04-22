b4850e33264b48a225fd20572e3030c884abbf5a
gracenak <gracenak@gmail.com>
2021-04-21 18:53:09 -0700

    add more validations to models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,6 @@
 class Recipe < ApplicationRecord
     has_many :reviews
     belongs_to :user
+    
+    validates :title, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
     has_many :recipes
     has_many :reviews, through: :recipes
 
+    validates :username, presence: true
+    validates :email, uniqueness: true
+
 end
